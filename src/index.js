2d96c395995b186ed6ff84da691e51fb5600340f
Giles Van Gruisen <gilesvangruisen@me.com>
2017-09-05 17:50:28 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -1,98 +1,81 @@
+function addSVG(element, attr) {
+  const svg = document.getElementById('svg')
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", element)
+
+  Object.keys(attr).forEach(key => {
+    path.setAttribute(key, attr[key])
+  })
+
+  svg.appendChild(path)
+}
+
+function markCurve({ x, y, cx, cy, x2, y2, cx2, cy2 }) {
+  const path = `M ${x} ${y}, C ${cx} ${cy}, ${cx2} ${cy2}, ${x2} ${y2}`
+
+  addSVG('path', {
+    d: path,
+    stroke: 'blue',
+    'stroke-width': 2,
+    fill: 'transparent'
+  })
+}
 
 function markPoint(point, color = 'red', size = 20) {
-  var c = document.createElement('div')
-    c.style = `
-    position: absolute;
-    top: ${point.y - (size / 2)}px;
-    left: ${point.x - (size / 2)}px;
-    background: ${color};
-    border-radius: ${size / 2}px;
-    width: ${size}px;
-    height: ${size}px;
-    `
+  const halfSize = size / 2
 
-    document.querySelector('body').appendChild(c)
+  addSVG('circle', {
+    cx: point.x,
+    cy: point.y,
+    r: halfSize,
+    fill: color
+  })
 }
-
 
 const points = []
-const fixedPoints = []
+const curves = []
 
-var timeoutTime = 260;
+var timeoutTime = 30;
 var stopTimeout;
+var pushTimeout;
 
-function onMouseMove(e) {
-  const now = Date.now()
+function onMouseMove(onStart, onSample, onStop) {
+  return function (e) {
+    const now = Date.now()
+    const lastPoint = points[points.length - 1]
+    const timeDelta = lastPoint ? now - lastPoint.t : null
 
-  let lastFixed = fixedPoints[fixedPoints.length - 1]
-  var lastPoint = points[points.length - 10]
+    const nextPoint = {
+      dt: timeDelta,
+      dx: e.movementX,
+      dy: e.movementY,
+      t: now,
+      x: e.clientX,
+      y: e.clientY,
+      vx: e.movementX / timeDelta,
+      vy: e.movementY / timeDelta,
+    }
 
-  if (lastFixed && lastPoint && lastFixed.t > lastPoint.t) {
-    lastPoint = lastFixed
+    points.push(nextPoint)
+
+    if (!lastPoint || timeDelta > timeoutTime) {
+      onStart(nextPoint)
+    }
+
+    let ax = (nextPoint.vx - lastPoint.vx) / timeDelta,
+        ay = (nextPoint.vy - lastPoint.vy) / timeDelta
+
+
+    console.log(ax, ay)
+
+    onSample(nextPoint)
+
+    stopTimeout && clearTimeout(stopTimeout)
+    stopTimeout = setTimeout(() => {
+      pushTimeout && clearTimeout(pushTimeout)
+      onStop(nextPoint)
+    }, timeoutTime)
   }
-
-  const timeDelta = lastPoint ? now - lastPoint.t : null
-
-  let dy, dx, slope, rad, drad = null
-
-  if (lastPoint) {
-    dy = e.clientY - lastPoint.y
-    dx = e.clientX - lastPoint.x
-    slope = (dy / dx)
-    rad = Math.atan2(dy, dx)
-    drad = rad - lastPoint.rad
-  }
-
-  let fdx, fdy, frad, fdrad = null
-
-  if (lastFixed) {
-    fdy = e.clientY - lastFixed.y
-    fdx = e.clientX - lastFixed.x
-    frad = Math.atan2(fdy, fdx)
-    fdrad = frad - lastFixed.frad
-  }
-
-  const nextPoint = {
-    dt: timeDelta,
-    dx: e.movementX,
-    dy: e.movementY,
-    t: now,
-    x: e.clientX,
-    y: e.clientY,
-    rad: rad,
-    drad: drad,
-    frad: frad,
-    fdrad: fdrad
-  }
-
-  if (!lastFixed) {
-    markPoint(nextPoint, 'green', 10)
-    markFixedPoint(nextPoint)
-  } else if (timeDelta > timeoutTime) {
-    markStart(nextPoint)
-  } else if (Math.abs(drad) > 0.75 && lastPoint.t > lastFixed.t) {
-    // markPoint(lastPoint, 'blue', 10)
-    // markFixedPoint(lastPoint)
-  }
-
-  points.push(nextPoint)
-
-  stopTimeout && clearTimeout(stopTimeout)
-  stopTimeout = setTimeout(function () {
-    onMouseStop(nextPoint)
-  }, timeoutTime)
-}
-
-function _markStart(point) {
-  markPoint(point, 'green', 10)
-  markFixedPoint(point)
-}
-
-const markStart = throttle(_markStart, 9)
-
-function onMouseStop(point) {
-  markPoint(point, 'rgba(255,0,0,0.25)')
-  markFixedPoint(point)
 }
 
 function markFixedPoint(point) {
@@ -101,17 +84,75 @@ function markFixedPoint(point) {
   markPoint(point, 'rgba(0,0,255,0.25)', 40)
 }
 
-function throttle(f, n) {
-  var count = 0
+class MouseProcessor {
+  constructor() {
+    this.curves = []
+    this.samples = []
+  }
 
-  return function () {
-    if (count === 0) {
-      f.apply(this, arguments)
-      count = n
-    } else {
-      count--
-    }
+  add(p) {
+    this.samples.push(p)
+  }
+
+  finish() {
+    if (this.samples.length < 2) { return }
+
+    const points = this.samples.slice()
+    this.samples = []
+
+    const curve = this._buildCurve(points)
+    this.curves.push(curve)
+
+    markCurve(curve)
+  }
+
+  _buildCurve(points) {
+    const gap = 1 // Math.floor(points.length / 5)
+    const factor = 1
+
+    const startP = points[0],
+          startP2 = points[gap],
+          endP = points[points.length - 1],
+          endP2 = points[points.length - gap - 1]
+
+    const x = startP.x,
+          y = startP.y,
+          dx = startP2.x - x,
+          dy = startP2.y - y,
+          cx = x + (dx * factor),
+          cy = y + (dy * factor)
+
+
+    const x2 = endP.x,
+          y2 = endP.y,
+          dx2 = x2 - endP2.x,
+          dy2 = y2 - endP2.y,
+          cx2 = x2 - (dx2 * factor),
+          cy2 = y2 - (dy2 * factor)
+
+    return { x, y, cx, cy, x2, y2, cx2, cy2 }
   }
 }
 
-window.onmousemove = onMouseMove
+const processor = new MouseProcessor()
+
+function onStart(p) {
+  markPoint(p, 'rgba(0,200,0,0.25)', 40)
+}
+
+function onSample(p) {
+  markPoint(p, 'rgba(100,100,100,0.25', 5)
+  processor.add(p)
+}
+
+function onStop(p) {
+  markPoint(p, 'rgba(200,0,0,0.25)', 20)
+
+  processor.finish()
+}
+
+
+
+
+
+window.onmousemove = onMouseMove(onStart, onSample, onStop)
