16dc74fe6206ddaaa9cbfd97286af0dbe89f2993
Giles Van Gruisen <gilesvangruisen@me.com>
2017-09-05 15:48:09 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -14,23 +14,43 @@ function markPoint(point, color = 'red', size = 20) {
     document.querySelector('body').appendChild(c)
 }
 
-const points = []
-var vel = 0
-var dir = null
 
-var timeoutTime = 20;
+const points = []
+const fixedPoints = []
+
+var timeoutTime = 260;
 var stopTimeout;
 
 function onMouseMove(e) {
-  stopTimeout && clearTimeout(stopTimeout)
-  stopTimeout = setTimeout(function () {
-    onMouseStop(e)
-  }, timeoutTime)
-
   const now = Date.now()
-  const lastPoint = points[points.length - 1]
+
+  let lastFixed = fixedPoints[fixedPoints.length - 1]
+  var lastPoint = points[points.length - 10]
+
+  if (lastFixed && lastPoint && lastFixed.t > lastPoint.t) {
+    lastPoint = lastFixed
+  }
 
   const timeDelta = lastPoint ? now - lastPoint.t : null
+
+  let dy, dx, slope, rad, drad = null
+
+  if (lastPoint) {
+    dy = e.clientY - lastPoint.y
+    dx = e.clientX - lastPoint.x
+    slope = (dy / dx)
+    rad = Math.atan2(dy, dx)
+    drad = rad - lastPoint.rad
+  }
+
+  let fdx, fdy, frad, fdrad = null
+
+  if (lastFixed) {
+    fdy = e.clientY - lastFixed.y
+    fdx = e.clientX - lastFixed.x
+    frad = Math.atan2(fdy, fdx)
+    fdrad = frad - lastFixed.frad
+  }
 
   const nextPoint = {
     dt: timeDelta,
@@ -38,35 +58,60 @@ function onMouseMove(e) {
     dy: e.movementY,
     t: now,
     x: e.clientX,
-    y: e.clientY
+    y: e.clientY,
+    rad: rad,
+    drad: drad,
+    frad: frad,
+    fdrad: fdrad
   }
 
-  if (timeDelta > timeoutTime) { markPoint(nextPoint, 'green', 10) }
-
-  const nextLastPoint = points[points.length - 10]
-
+  if (!lastFixed) {
+    markPoint(nextPoint, 'green', 10)
+    markFixedPoint(nextPoint)
+  } else if (timeDelta > timeoutTime) {
+    markStart(nextPoint)
+  } else if (Math.abs(drad) > 0.75 && lastPoint.t > lastFixed.t) {
+    // markPoint(lastPoint, 'blue', 10)
+    // markFixedPoint(lastPoint)
+  }
 
   points.push(nextPoint)
+
+  stopTimeout && clearTimeout(stopTimeout)
+  stopTimeout = setTimeout(function () {
+    onMouseStop(nextPoint)
+  }, timeoutTime)
 }
 
-function onMouseStop(e) {
-  const lastPoint = points[points.length - 1]
-  const thirdLastPoint = points[points.length - 3]
-
-  if (!lastPoint) return
-
-  markPoint(lastPoint)
-
-  if (!thirdLastPoint) return
-  // lastPoint && console.log(lastPoint.dx, lastPoint.dy)
+function _markStart(point) {
+  markPoint(point, 'green', 10)
+  markFixedPoint(point)
 }
 
-// function mouseTick() {
-//   const point = points[points.length - 1]
-//   point && console.log(point)
-// }
+const markStart = throttle(_markStart, 9)
 
-// setInterval(mouseTick, 10)
+function onMouseStop(point) {
+  markPoint(point, 'rgba(255,0,0,0.25)')
+  markFixedPoint(point)
+}
 
+function markFixedPoint(point) {
+  fixedPoints.push(point)
+
+  markPoint(point, 'rgba(0,0,255,0.25)', 40)
+}
+
+function throttle(f, n) {
+  var count = 0
+
+  return function () {
+    if (count === 0) {
+      f.apply(this, arguments)
+      count = n
+    } else {
+      count--
+    }
+  }
+}
 
 window.onmousemove = onMouseMove
